ce1f96fcd20b8e0c9abbecda7b3dd408692cfe37
thu <ndttran.2019@phdcs.smu.edu.sg>
2025-01-29 22:30:08 +0800

    add stream mode

--- config/general.py
+++ config/general.py
@@ -1,3 +1,4 @@
+MODE = 'STREAMING' # MODE is VIDEO or STREAMING
 # Model
 POSE_CONFIG = 'config/pose_model.yaml'
 SEG_CONFIG = 'config/seg_model.yaml'
@@ -7,12 +8,16 @@ FACE_CONFIG = 'config/face_model.yaml'
 SAVE_AFTER_SECONDS = 5 
 SAVE_JSON_PATH = 'frame_info/interval_{}.json'
 
-# For VIDEO mode
-MODE = 'VIDEO'
+# For VIDEO
 FILENAME = 'DJI_0965.MP4'
 VIDEO_PATH = f'2024Mar/{FILENAME}' 
-# SWIMMER_POSITION_X = 700 
-# SWIMMER_POSITION_Y = 300 
-SAVE_CSV_PATH = f'csv_files/{FILENAME.split('.')[0]}.csv'
-
-
+# For STREAMING
+DEVICE_ID = 0  
+if MODE == 'VIDEO':
+    SAVE_CSV_PATH = f'csv_files/{FILENAME.split('.')[0]}.csv'
+    SAVE_VIDEO_PATH = f'{FILENAME.split('.')[0]}_output.mp4'
+elif MODE == 'STREAMING':
+    from datetime import datetime
+    current_date_time = datetime.today().strftime('%Y%m%d_%H%M%S')
+    SAVE_CSV_PATH = f'csv_files/{current_date_time}.csv'
+    SAVE_VIDEO_PATH = f'{current_date_time}_output.mp4'
--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from config.general import MODE, SAVE_AFTER_SECONDS, SAVE_JSON_PATH, SAVE_CSV_PATH, VIDEO_PATH
+from config.general import MODE, SAVE_AFTER_SECONDS, SAVE_JSON_PATH, SAVE_CSV_PATH, VIDEO_PATH, DEVICE_ID, SAVE_VIDEO_PATH
 import cv2 
 from utils.file_utils import write_to_csv
 from postprocess.data import FrameData
 from typing import List
 from main_process.main_processing import MainCalculation
 import os
+import sys
 import logging 
 logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                     level=logging.DEBUG,
@@ -12,7 +13,7 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
 
 frame_data_list: List[FrameData] = []
 
-def run_video(debug=False, save_json=False, save_csv=False, out_video='output.mp4', fx=1, fy=1):
+def run_video(debug=False, save_json=False, save_csv=False, out_video=SAVE_VIDEO_PATH, fx=1, fy=1):
     cap = cv2.VideoCapture(VIDEO_PATH)
     frame_width, frame_height = int(cap.get(3)*fx), int(cap.get(4)*fy)
     if frame_width == 0 and frame_height == 0:
@@ -65,11 +66,72 @@ def run_video(debug=False, save_json=False, save_csv=False, out_video='output.mp
         logging.debug(f'Csv file is saved at {SAVE_CSV_PATH}')
 
 
+def run_stream(debug=False, save_json=False, save_csv=False, out_video=SAVE_VIDEO_PATH, fx=1, fy=1):
+    cap = cv2.VideoCapture(DEVICE_ID)
+    frame_width, frame_height = int(cap.get(3)*fx), int(cap.get(4)*fy)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    frame_idx = 0
+    output = cv2.VideoWriter(out_video, cv2.VideoWriter_fourcc(*'MP4V'),
+                             fps, (frame_width, frame_height))
+    
+    logging.info(f'frame_width={frame_width}, frame_height={frame_width}, fps = {fps}')
+
+    if save_csv:
+        if os.path.exists(SAVE_CSV_PATH):
+            os.remove(SAVE_CSV_PATH)
+    
+    calculate_frame = MainCalculation(fps)
+    
+    while cap.isOpened():
+        try:
+            ret, frame = cap.read()
+            if ret:
+                out_frame, frame_data = calculate_frame.swimming_calculation(frame=frame, frame_idx=frame_idx, debug=debug)
+                frame_idx += 1
+                
+                # save SAVE_AFTER_SECONDS frames in json format
+                if frame_idx % (SAVE_AFTER_SECONDS*fps) == 0:
+                    if save_json:
+                        filename = SAVE_JSON_PATH.format(frame_idx//(SAVE_AFTER_SECONDS*fps))
+                        save_json([_frame.__dict__ for _frame in frame_data_list[frame_idx-(SAVE_AFTER_SECONDS*fps):]], filename)
+                        logging.info(f'Saved json file to {filename}')
+                    else:
+                        logging.debug('>>>>> {} seconds elapsed'.format(SAVE_AFTER_SECONDS))
+                        
+                if save_csv:
+                    data = str(frame_data.red_marker) + ',' + str(frame_data.direction) + ',' + str(frame_data.speed) + ',' + ','.join(str(p) for p in frame_data.skeleton)
+                    write_to_csv(data, SAVE_CSV_PATH)
+
+                
+                if out_video:
+                    output.write(out_frame)
+                
+            else:
+                if save_csv:
+                    logging.info(f'Saved csv file to {SAVE_CSV_PATH}')
+
+                cap.release()
+                output.release()
+        except KeyboardInterrupt:
+            cap.release()
+            output.release()
+            if save_csv:
+                logging.info(f'Saved csv file to {SAVE_CSV_PATH}')
+            logging.info(f'Saved video to {out_video}')
+            sys.exit()
+    
+    if debug:
+        logging.debug(f'Annotated video is saved at {out_video}')
+    if save_csv:
+        logging.debug(f'Csv file is saved at {SAVE_CSV_PATH}')
+
+
+
 if __name__ == '__main__':
-    """ run the input video frame by frame and save the frame info as json files
-    """
     if MODE == 'VIDEO':
         run_video(debug=False, save_json=False, save_csv=True)
+    elif MODE == 'STREAMING':
+        run_stream(debug=False, save_json=False, save_csv=True)
     
     
    
