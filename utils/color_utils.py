902abeff6958734858ea07795ec3778c384a99da
thu <ndttran.2019@phdcs.smu.edu.sg>
2025-03-13 16:37:58 +0800

    reverse

--- utils/color_utils.py
+++ utils/color_utils.py
@@ -53,7 +53,6 @@ def filter_red(image: np.ndarray) -> np.ndarray:
     mask1 = cv2.inRange(img_hsv, lower_red, upper_red)
     
     mask = mask0+mask1
-    return mask 
     if np.all(mask==0): return np.array([])
     return crop_image_from_mask(image, mask)
 
@@ -64,8 +63,6 @@ def filter_blue(image: np.ndarray) -> np.ndarray:
     lower_blue = np.array([90, 0, 0]) 
     upper_blue = np.array([130, 255, 255])
     mask = cv2.inRange(img_hsv, lower_blue, upper_blue)
-    return mask
-    
     if np.all(mask==0): return np.array([])
     return crop_image_from_mask(image, mask)
  
@@ -77,8 +74,6 @@ def filter_yellow(image: np.ndarray) -> np.ndarray:
     lower_yellow = np.array([12, 0, 0])
     upper_yellow = np.array([32, 255, 255])
     mask = cv2.inRange(img_hsv, lower_yellow, upper_yellow)
-    return mask
-    
     if np.all(mask==0): return np.array([])
     return crop_image_from_mask(image, mask)
 
