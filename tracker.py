26adfe60a68fc50c5359f7f5305e34de80fe5554
thu <doanthu249@gmail.com>
2026-01-19 21:50:09 +0800

    clean repo

--- tracker.py
+++
@@ -1,8 +0,0 @@
-from ultralytics import YOLO
-
-# Load an official or custom model
-model = YOLO("models/pose-1080-2025.pt")  # Load an official Pose model
-
-# Perform tracking with the model
-results = model.track("DJI_0304_resized.MP4", show=True, verbose=False, save=True, imgsz=640)  # Tracking with default tracker
-# results = model.track("https://youtu.be/LNwODJXcvt4", show=True, tracker="bytetrack.yaml")  # with ByteTrack
